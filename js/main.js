7e93d50f48b24d6b37ed252f18c69f87d7507118
Chairil Rafi Purnama <chairilrafi11@gmail.com>
2025-11-19 11:56:14 +0700

    content : photo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY ANNISA PRAMESWARI').split('')
+    const titles = ('HAPPY BIRTHDAY ANNISA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
